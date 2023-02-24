6b9ab7fafcab5e2540e298fdd0ef2d75d8c16cfd
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-02-25 00:30:04 +0100

    Create Image component

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Pokemon from "../../assets/pokemon.svg";
+
+export const Image = () => {
+  return (
+    <div className="mb-20">
+      <img
+        src={Pokemon}
+        alt="pokemon"
+        loading="lazy"
+        sizes="(max-width: 600px) 100vw, 50vw"
+      />
+    </div>
+  );
+};
