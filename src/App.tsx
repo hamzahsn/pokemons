a9c7a660fe69dec7aae85b9c3b98b2c0a082c9fe
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-02-25 00:29:02 +0100

    Wrap router with components and give each its own routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { Home, PokemonDetail, Favorites } from "./pages";
+import { Header } from "./components";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="pokemon/:name" element={<PokemonDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </div>
   );
 }
