f8d34eeb692a1484807c00abdd2cd03dba9e4481
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-02-25 00:31:47 +0100

    Add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../../assets/logo.svg";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Pokemon Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Pokemon
+            </span>
+          </Link>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/favorites"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  My Favorites
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
