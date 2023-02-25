bb05e12b194a08ae55a39080a922a1829d4f1dd5
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-02-25 01:51:05 +0100

    Create Favorite page

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+import { PokemonCard } from "../../components";
+import { PokemonContext } from "../../context/pokemonContext";
+import "./Favorites.css";
+
 export const Favorites = () => {
-  return <div>My favorite Pokemons</div>;
+  const { pokemons } = React.useContext(PokemonContext);
+
+  return (
+    <div className="wrapper">
+      <h1 className="text-3xl font-bold text-left">My Favorite Pokemons</h1>
+      <div className="grid lg:grid-cols-4 sm:grid-cols-1 sm:gap-6 md:grid-cols-2 md:gap-2">
+        {pokemons &&
+          pokemons.length > 0 &&
+          pokemons.map((myFavoritesPokemons, index) => (
+            <PokemonCard key={index} {...myFavoritesPokemons} />
+          ))}
+      </div>
+    </div>
+  );
 };
