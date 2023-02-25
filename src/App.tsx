da70153e041de9684a3f90a7b7f5a15a418610f7
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-02-25 01:50:11 +0100

    Wrap app component with pokemon context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import { PokemonContextProvider } from "./context/pokemonContext";
 import { Home, PokemonDetail, Favorites } from "./pages";
 import { Header } from "./components";
 
@@ -10,11 +11,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="pokemon/:name" element={<PokemonDetail />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
+      <PokemonContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="pokemon/:name" element={<PokemonDetail />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </PokemonContextProvider>
     </div>
   );
 }
