ca731097a7a050cb9f85ebec69ee4ffd87bc549b
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-03-08 16:51:34 +0100

    Create different pages

---
+++ solid/src/pages/Favorites/index.tsx
@@ -0,0 +1,9 @@
+function Favorites() {
+  return (
+    <div>
+      <h1>Favorites</h1>
+    </div>
+  );
+}
+
+export default Favorites;
---
+++ solid/src/pages/PokemonDetails/index.tsx
@@ -0,0 +1,9 @@
+function PokemonDetails() {
+  return (
+    <div>
+      <h1>PokemonDetails</h1>
+    </div>
+  );
+}
+
+export default PokemonDetails;
