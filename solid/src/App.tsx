814325ce3bf667754328c11f1e0fd0bc4242cf4a
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-03-08 18:48:57 +0100

    Add basic routing config

--- solid/src/App.tsx
+++ solid/src/App.tsx
@@ -2,8 +2,13 @@ import { lazy } from "solid-js";
 import { useRoutes } from "@solidjs/router";
 
 import styles from "./App.module.css";
+import { Header } from "./components";
 
 const routes = [
+  {
+    path: "/",
+    component: lazy(() => import("./pages/Home")),
+  },
   {
     path: "/pokemon/:name",
     component: lazy(() => import("./pages/PokemonDetails")),
@@ -12,19 +17,17 @@ const routes = [
     path: "/favorites",
     component: lazy(() => import("./pages/Favorites")),
   },
-  {
-    path: "/",
-    component: import("./pages/Home"),
-  },
 ];
 
 function App() {
   const Routes = useRoutes(routes);
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>hello</header>
-      <Routes />
-    </div>
+    <>
+      <Header />
+      <div class={styles.App}>
+        <Routes />
+      </div>
+    </>
   );
 }
 
