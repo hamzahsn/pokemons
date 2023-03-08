8a7e483b709e24ecc66554e75d4e433356362c23
hamzahsn <amdouni.hamza.hsn@gmail.com>
2023-03-08 18:48:25 +0100

    Add global css styles for tailwind

--- solid/src/index.tsx
+++ solid/src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
 
 import "./index.css";
+import "./styles/global.css";
 import App from "./App";
 
 const root = document.getElementById("root");
